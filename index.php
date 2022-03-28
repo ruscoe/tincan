00f6b6bde0ddeb60db2df301ae4fc7dc476190fe
ruscoe <danruscoe@gmail.com>
2022-03-28 13:08:45 -0700

    Add boards to front page.

--- index.php
+++ index.php
@@ -6,18 +6,24 @@ require 'includes/include-db.php';
 require 'includes/include-objects.php';
 require 'includes/include-template.php';
 
-// Test posts data.
-$posts = array(
-  (object) array(
-    'id' => 1,
-    'title' => 'Aut velit non ea perspiciatis ex',
-    'url' => '/posts/1'
-  )
-);
+$db = new TCData();
 
-foreach ($posts as $post) {
+$board_groups = $db->load_objects(new TCBoardGroup());
 
-  $data = array('post' => $post);
-  TCTemplate::render('post-preview', $data);
+foreach ($board_groups as $group) {
+  $board_conditions = array(
+    array(
+      'field' => 'board_group_id',
+      'value' => $group->board_group_id
+    )
+  );
 
+  $boards = $db->load_objects(new TCBoard(), array(), $board_conditions);
+
+  $data = array(
+    'board_group' => $group,
+    'boards' => $boards
+  );
+
+  TCTemplate::render('board-group', $data);
 }
---
+++ templates/board-group.php
@@ -0,0 +1,15 @@
+<?php
+  $board_group = $data['board_group'];
+  $boards = $data['boards'];
+?>
+
+<div id="board-group-<?=$board_group->board_group_id?>" class="board-group">
+  <h2><a href="#"><?=$board_group->board_group_name?></a></h2>
+  <ul>
+    <?php foreach ($boards as $board) { ?>
+      <li>
+        <h3><?=$board->board_name?></h3>
+      </li>
+    <?php } ?>
+  </ul>
+</div>
