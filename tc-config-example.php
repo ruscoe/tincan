22722b349458e0c28debf3740cb7fc274d1a2909
ruscoe <danruscoe@gmail.com>
2022-03-28 13:07:44 -0700

    Add database configuration to example config file.

--- tc-config-example.php
+++ tc-config-example.php
@@ -1,3 +1,9 @@
 <?php
 
 define('TC_BASE_PATH', '/var/www/YOUR_SITE');
+
+define('TC_DB_CLASS', 'TCMySQL');
+define('TC_DB_HOST', 'http://localhost');
+define('TC_DB_USER', 'root');
+define('TC_DB_PASS', 'root');
+define('TC_DB_NAME', 'tincan');
