cbf7324fa46d17323e59308eee8508f9aff08037
ruscoe <danruscoe@gmail.com>
2022-03-27 21:28:25 -0700

    Add initial data objects.

---
+++ includes/include-objects.php
@@ -0,0 +1,6 @@
+<?php
+
+require TC_BASE_PATH . '/objects/board.php';
+require TC_BASE_PATH . '/objects/post.php';
+require TC_BASE_PATH . '/objects/thread.php';
+require TC_BASE_PATH . '/objects/user.php';
