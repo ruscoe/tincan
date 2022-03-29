1467a2624cf97f78ff542d61367b5879f454e56d
ruscoe <danruscoe@gmail.com>
2022-03-28 18:04:26 -0700

    Include new page object.

--- includes/include-objects.php
+++ includes/include-objects.php
@@ -3,6 +3,7 @@
 require TC_BASE_PATH . '/core/objects/class-tc-object.php';
 require TC_BASE_PATH . '/core/objects/class-tc-board-group.php';
 require TC_BASE_PATH . '/core/objects/class-tc-board.php';
+require TC_BASE_PATH . '/core/objects/class-tc-page.php';
 require TC_BASE_PATH . '/core/objects/class-tc-post.php';
 require TC_BASE_PATH . '/core/objects/class-tc-thread.php';
 require TC_BASE_PATH . '/core/objects/class-tc-user.php';
