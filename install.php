00c6ced6e885414874f0327497d2aee9a0831d45
ruscoe <danruscoe@gmail.com>
2022-04-04 22:03:30 -0700

    Update installer to generate a custom amount of content.

--- install.php
+++ install.php
@@ -7,6 +7,37 @@ require TC_BASE_PATH . '/includes/include-objects.php';
 require TC_BASE_PATH . '/includes/include-template.php';
 require TC_BASE_PATH . '/includes/include-user.php';
 
+function get_random_lipsum_short() {
+  $lipsum = array(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'Nullam euismod faucibus ipsum, a porttitor odio eleifend eget.',
+    'Donec et placerat nunc, quis tempus ipsum.',
+    'Cras suscipit eros quis mauris cursus, vitae commodo lacus feugiat.',
+    'Duis sed ipsum quis libero aliquam vestibulum et in quam.',
+    'Aenean at dui vel dui aliquam venenatis et vitae turpis.'
+  );
+
+  $index = rand(0, (count($lipsum) - 1));
+
+  return $lipsum[$index];
+}
+
+function get_random_lipsum_long() {
+  $lipsum = array(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie nisi vel mi eleifend tincidunt. Ut dictum ornare quam ut imperdiet. Mauris justo ante, ullamcorper sed libero ac, tincidunt pellentesque sapien. Fusce ut vulputate libero, id mollis felis. Nullam euismod faucibus ipsum, a porttitor odio eleifend eget. Etiam sed lectus eu magna vestibulum finibus a et nunc. Fusce sit amet metus varius, malesuada mauris non, gravida urna.',
+    'Nullam ut libero tellus. Quisque vel elementum metus, a pretium dolor. Nullam quis auctor enim. Nunc sagittis tincidunt sagittis. Nulla arcu urna, volutpat ut urna quis, suscipit auctor elit. Donec et placerat nunc, quis tempus ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mollis dolor ac mi egestas, sit amet porta sem consequat. Etiam molestie purus felis, a convallis sem consequat eu. Duis facilisis pellentesque.',
+    'Vivamus tempor euismod aliquam. Fusce tempus ultrices vestibulum. Praesent a sagittis justo. Sed ut molestie eros. Curabitur varius dolor non augue lobortis, id ullamcorper elit faucibus. Fusce pulvinar dictum diam id placerat. Vestibulum mattis enim ac tortor tincidunt, quis vulputate diam vehicula. Mauris euismod dui at elit congue pulvinar.',
+    'Aenean ullamcorper at mi eget mollis. Cras in varius tellus. Praesent pretium, tortor mattis faucibus volutpat, nisl diam pharetra diam, et condimentum eros dui lobortis urna. Donec id velit at lorem ultricies volutpat sit amet at tortor. Aenean at dui vel dui aliquam venenatis et vitae turpis. In finibus mollis lectus non efficitur.',
+    'Etiam molestie purus felis, a convallis sem consequat eu. Duis facilisis pellentesque dolor nec vehicula. Morbi pulvinar porta erat gravida commodo. Fusce enim turpis, laoreet ac dapibus nec, eleifend sit amet ipsum. Nunc eget libero lacinia enim interdum gravida. Nam gravida ut urna in dignissim.',
+    'Phasellus suscipit sagittis lorem, nec tristique libero porta id. Vestibulum dictum libero eget augue blandit ultrices. Vestibulum a massa nulla. Suspendisse bibendum ante ac diam dictum, at laoreet est lobortis. Mauris facilisis lacinia purus, nec pharetra nisi ullamcorper vulputate.'
+  );
+
+  $index = rand(0, (count($lipsum) - 1));
+
+  return $lipsum[$index];
+}
+
+
 $db = new TCData();
 
 // === Truncate tables ===
@@ -111,55 +142,75 @@ foreach ($users as $user) {
 
 // === Create board groups ===
 
-$board_groups = array(
-  array('board_group_name' => 'Programming'),          // 1
-  array('board_group_name' => 'Modal Building'),       // 2
-  array('board_group_name' => 'Comic Books and Cards') // 3
-);
+$board_groups_to_create = 4;
+
+for ($i = 0; $i <= $board_groups_to_create; $i++) {
+  $board_groups[] = array('board_group_name' => get_random_lipsum_short());
+}
+
+$new_board_group_ids = array();
 
 foreach ($board_groups as $board_group) {
   $board_group['created_time'] = time();
   $board_group['updated_time'] = time();
-  $db->save_object(new TCBoardGroup((object) $board_group));
+  $new_board_group = $db->save_object(new TCBoardGroup((object) $board_group));
+
+  $new_board_group_ids[] = $new_board_group->board_group_id;
 }
 
 // === Create boards ===
 
-$boards = array(
-  array('board_group_id' => 1, 'board_name' => 'General Programming'), // 1
-  array('board_group_id' => 1, 'board_name' => 'Web Programming'),     // 2
-  array('board_group_id' => 1, 'board_name' => 'Game Programming'),    // 3
-  array('board_group_id' => 2, 'board_name' => 'Airfix'),              // 4
-  array('board_group_id' => 2, 'board_name' => 'Miniature Painting'),  // 5
-  array('board_group_id' => 2, 'board_name' => '3D Printing'),         // 6
-  array('board_group_id' => 3, 'board_name' => 'Indie Comics'),        // 7
-  array('board_group_id' => 3, 'board_name' => 'Marvel / DC'),         // 8
-  array('board_group_id' => 3, 'board_name' => 'Trading Cards')        // 9
-);
+$board_groups_to_create = 4;
+
+foreach ($new_board_group_ids as $board_group_id) {
+  for ($i = 0; $i <= $board_groups_to_create; $i++) {
+    $boards[] = array('board_group_id' => $board_group_id, 'board_name' => get_random_lipsum_short(), 'description' => get_random_lipsum_short());
+  }
+}
+
+$new_board_ids = array();
 
 foreach ($boards as $board) {
   $board['created_time'] = time();
   $board['updated_time'] = time();
-  $db->save_object(new TCBoard((object) $board));
+  $new_board = $db->save_object(new TCBoard((object) $board));
+
+  $new_board_ids[] = $new_board->board_id;
 }
 
 // === Create threads ===
 
-$threads = array(
-  array('board_id' => 1, 'thread_title' => 'My favorite part of the Fibonacci sequence') // 1
-);
+$threads_to_create = 12;
+
+$threads = array();
+
+foreach ($new_board_ids as $board_id) {
+  for ($i = 0; $i <= $threads_to_create; $i++) {
+    $threads[] = array('board_id' => $board_id, 'thread_title' => get_random_lipsum_short());
+  }
+}
+
+$new_thread_ids = array();
 
 foreach ($threads as $thread) {
   $thread['created_time'] = time();
   $thread['updated_time'] = time();
-  $db->save_object(new TCThread((object) $thread));
+  $new_thread = $db->save_object(new TCThread((object) $thread));
+
+  $new_thread_ids[] = $new_thread->thread_id;
 }
 
 // === Create posts ===
 
-$posts = array(
-  array('user_id' => 1, 'thread_id' => 1, 'content' => '8, 13, 21, 34, 55, 89')
-);
+$posts_to_create = 24;
+
+$posts = array();
+
+foreach ($new_thread_ids as $thread_id) {
+  for ($i = 0; $i <= $posts_to_create; $i++) {
+    $posts[] = array('user_id' => 1, 'thread_id' => $thread_id, 'content' => get_random_lipsum_long());
+  }
+}
 
 foreach ($posts as $post) {
   $post['created_time'] = time();
