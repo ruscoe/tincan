32e2cf1005788876a53f49ee8024770ad7dac6f7
ruscoe <danruscoe@gmail.com>
2022-04-05 17:00:59 -0700

    Add basic pagination to threads.

--- templates/page/thread.php
+++ templates/page/thread.php
@@ -1,8 +1,9 @@
 <?php
 
 $thread_id = filter_input(INPUT_GET, 'thread', FILTER_SANITIZE_NUMBER_INT);
-$page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_NUMBER_INT);
+$start_at = filter_input(INPUT_GET, 'start_at', FILTER_SANITIZE_NUMBER_INT);
 
+$page = $data['page'];
 $settings = $data['settings'];
 
 $db = new TCData();
@@ -26,10 +27,18 @@ $order = array(
   'direction' => 'ASC'
 );
 
-$offset = 0;
+// TODO: Set bounds for offset so nothing crazy happens.
+//   Calculate maximum page number from posts.
+//   Avoid negative numbers.
+$offset = ($start_at > 1) ? $start_at : 0;
+$offset *= $settings['posts_per_page'];
 $limit = $settings['posts_per_page'];
 
-// TODO: Sorting and pagination.
+$total = $db->count_objects(new TCPost(), $conditions);
+// Calculate total pages, rounding up to ensure we can reach all posts.
+// TODO: This may need to be refined; ok for now.
+$total_pages = ($total <= $settings['posts_per_page']) ? 1 : ceil($total / $settings['posts_per_page']);
+
 $posts = $db->load_objects(new TCPost(), array(), $conditions, $order, $offset, $limit);
 
 foreach ($posts as $post) {
@@ -37,3 +46,10 @@ foreach ($posts as $post) {
 
   TCTemplate::render('post', array('post' => $post, 'user' => $user, 'settings' => $data['settings']));
 }
+
+$page_params = array(
+  'page' => $page->page_id,
+  'thread' => $thread->thread_id
+);
+
+TCTemplate::render('pagination', array('page_params' => $page_params, 'start_at' => $start_at, 'total_pages' => $total_pages, 'settings' => $data['settings']));
---
+++ templates/pagination.php
@@ -0,0 +1,21 @@
+<?php
+$start_at = $data['start_at'];
+$page_params = $data['page_params'];
+$total_pages = $data['total_pages'];
+
+$base_url = '?';
+
+foreach ($page_params as $name => $value) {
+  $base_url .= $name . '=' . $value . '&';
+}
+?>
+
+<div class="pagination">
+  <ul>
+    <?php
+    for ($i = 1; $i <= $total_pages; $i++) {
+      echo "<li><a href=\"{$base_url}start_at={$i}\">{$i}</a></li>";
+    }
+    ?>
+  </ul>
+</div>
