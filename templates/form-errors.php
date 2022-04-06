2397651d3646828a35249ec2192713cdb2ce428b
ruscoe <danruscoe@gmail.com>
2022-04-06 10:21:26 -0700

    Add form errors template.

---
+++ templates/form-errors.php
@@ -0,0 +1,30 @@
+<?php
+$errors = $data['errors'];
+?>
+
+<div class="message-box errors">
+  <ul>
+    <?php
+    foreach ($errors as $error_code) {
+      $error_text = '';
+
+      switch ($error_code) {
+        case TCObject::ERR_NOT_FOUND:
+        case TCObject::ERR_NOT_SAVED:
+          $error_text = 'TODO: ' . $error_code;
+          break;
+        case TCUser::ERR_USER:
+          $error_text = 'TODO: ' . $error_code;
+          break;
+        case TCUser::ERR_PASSWORD:
+          $error_text = 'TODO: ' . $error_code;
+          break;
+        default:
+          $error_text = 'A general error has occurred. Please try again later.';
+      }
+
+      echo "<li>{$error_text}</li>";
+    }
+    ?>
+  </ul>
+</div>
