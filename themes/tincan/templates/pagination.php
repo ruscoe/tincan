44569861f1c174af98def6d0bdb638d356290b30
ruscoe <danruscoe@gmail.com>
2022-04-24 08:53:46 -0700

    Unlink current page in pagination template.

--- themes/tincan/templates/pagination.php
+++ themes/tincan/templates/pagination.php
@@ -9,7 +9,7 @@ use TinCan\TCURL;
  *
  * @author Dan Ruscoe danruscoe@protonmail.com
  */
-$start_at = $data['start_at'];
+$start_at = (!empty($data['start_at'])) ? $data['start_at'] : 1;
 $page_params = $data['page_params'];
 $total_pages = $data['total_pages'];
 
@@ -28,7 +28,12 @@ $base_url = TCURL::create_url($page, $page_params);
   // TODO: Show selection of first / last page numbers with ellipses in the middle.
   // Avoid massive list of pages.
   for ($i = 1; $i <= $total_pages; ++$i) {
-    echo "<li><a href=\"{$base_url}&start_at={$i}\">{$i}</a></li>";
+    if ($i == $start_at) {
+      echo "<li>{$i}</li>";
+    }
+    else {
+      echo "<li><a href=\"{$base_url}&start_at={$i}\">{$i}</a></li>";
+    }
   }
   if ($start_at < $total_pages) {
     echo "<li><a href=\"{$base_url}&start_at=".($start_at + 1).'">Next</a></li>';
