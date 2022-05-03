6056f8e2bacde1f921a8fa7f4f0ea1986181b887
ruscoe <danruscoe@gmail.com>
2022-05-03 05:10:29 -0700

    Add mail handler.

---
+++ core/class-tc-mailer.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace TinCan;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+/**
+ * Tin Can mail handler.
+ *
+ * @since 0.07
+ *
+ * @author Dan Ruscoe danruscoe@protonmail.com
+ */
+class TCMailer
+{
+  /**
+   * @since 0.07
+   */
+  private PHPMailer $mailer;
+
+  public function __construct($enable_exceptions = true)
+  {
+    $this->mailer = new PHPMailer($enable_exceptions);
+
+    // Enable SMTP.
+    $this->mailer->isSMTP();
+    $this->mailer->SMTPAuth = true;
+
+    if (TC_SMTP_DEBUG) {
+      $this->mailer->SMTPDebug = SMTP::DEBUG_SERVER;
+    }
+
+    if (TC_SMTP_TLS) {
+      $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    }
+
+    $this->mailer->Host = TC_SMTP_HOST;
+    $this->mailer->Username = TC_SMTP_USER;
+    $this->mailer->Password = TC_SMTP_PASS;
+    $this->mailer->Port = TC_SMTP_PORT;
+  }
+
+  /**
+   * Sends an email.
+   *
+   * TODO: Add email templates.
+   *
+   * @since 0.07
+   *
+   * @param array $recipients the email addresses to send email to
+   *
+   * @return bool true if email was successfully sent
+   */
+  public function send_mail($from_name, $from_email, $recipients)
+  {
+    var_dump($recipients);
+
+    $this->mailer->setFrom($from_email, $from_name);
+
+    foreach ($recipients as $recipient_email) {
+      $this->mailer->addAddress($recipient_email);
+    }
+
+    $this->mailer->isHTML(false);
+    $this->mailer->Subject = 'Tin Can Forum Test Email';
+    $this->mailer->Body = 'Hello! Here is an email from Tin Can Forum!';
+
+    $this->mailer->send();
+
+    return true;
+  }
+}
--- tc-config-example.php
+++ tc-config-example.php
@@ -2,19 +2,20 @@
 
 define('TC_VERSION', '0.07');
 
-/** File paths **/
+/* File paths * */
 
 define('TC_BASE_PATH', '/var/www/YOUR_SITE');
 define('TC_UPLOADS_PATH', '/var/www/YOUR_SITE/uploads');
 
-/** Database server **/
+/* Database server * */
+
 define('TC_DB_CLASS', 'TinCan\TCMySQL');
 define('TC_DB_HOST', 'http://localhost');
 define('TC_DB_USER', 'root');
 define('TC_DB_PASS', 'root');
 define('TC_DB_NAME', 'tincan');
 
-/** Mail server **/
+/* Mail server * */
 
 // https://github.com/PHPMailer/PHPMailer#a-simple-example
 
