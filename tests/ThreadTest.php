5e5146613ed9e53b9311e4c7d886a78ac662e28e
ruscoe <danruscoe@gmail.com>
2022-05-20 16:09:56 -0700

    Add thread unit tests.

---
+++ tests/ThreadTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once TC_BASE_PATH.'/includes/include-db.php';
+require_once TC_BASE_PATH.'/includes/include-objects.php';
+
+use PHPUnit\Framework\TestCase;
+use TinCan\TCBoard;
+use TinCan\TCData;
+use TinCan\TCPost;
+use TinCan\TCThread;
+
+class ThreadTest extends TestCase
+{
+  public function testPostCanBeDeleted()
+  {
+    $post = new TCPost();
+    $post->post_id = 1;
+
+    $thread = new TCThread();
+    $thread->thread_id = 1;
+    $thread->first_post_id = $post->post_id;
+
+    // The first post in a thread cannot be deleted.
+    $this->assertFalse($thread->post_can_be_deleted($post));
+
+    $thread->first_post_id = 2;
+
+    $this->assertTrue($thread->post_can_be_deleted($post));
+  }
+}
