f51754576031ce9d660a53549b9fb68ecf57fc79
ruscoe <danruscoe@gmail.com>
2022-07-14 08:55:48 -0700

    Add permission check to post edit.

--- themes/tincan/templates/page/edit-post.php
+++ themes/tincan/templates/page/edit-post.php
@@ -24,6 +24,12 @@ $user = $data['user'];
 
 $db = new TCData();
 
+// Check user has permission to edit this post.
+if (empty($user) || !$user->can_edit_post($post)) {
+  header('Location: '.TCURL::create_url($settings['page_404']));
+  exit;
+}
+
 $post = $db->load_object(new TCPost(), $post_id);
 
 if (empty($post)) {
