aecef0499e11df257f1bae43633985be4d19b129
ruscoe <danruscoe@protonmail.com>
2023-04-14 17:06:31 -0700

    Replace compass with node sass package.

--- themes/tincan-example/config.rb
+++
@@ -1,5 +0,0 @@
-# Compass configuration for the Tin Can theme.
-# Compass compiles SASS to CSS.
-# Run `compass compile` from the same directory as this file to generate CSS.
-css_dir = "css"
-sass_dir = "sass"
