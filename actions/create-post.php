56dbc9636e9ac284c0832e1cd24c5eae31573a74
ruscoe <danruscoe@protonmail.com>
2024-08-09 19:33:20 -0700

    Add attachments when posts are created.

--- actions/create-post.php
+++ actions/create-post.php
@@ -15,6 +15,29 @@ require getenv('TC_BASE_PATH').'/vendor/autoload.php';
 $thread_id = filter_input(INPUT_POST, 'thread_id', FILTER_SANITIZE_NUMBER_INT);
 $post_content = filter_input(INPUT_POST, 'post_content', FILTER_SANITIZE_STRING);
 
+$attachments = [];
+
+if (!empty($_FILES['attachments'])) {
+    $total_files = count($_FILES['attachments']['name']);
+
+    // Reformat the files array to individual files.
+    if ($total_files > 0) {
+        $attachments = [];
+        for ($i = 0; $i < $total_files; $i++) {
+            // Skip empty files.
+            if (empty($_FILES['attachments']['name'][$i])) {
+                continue;
+            }
+
+            $file = [];
+            foreach ($_FILES['attachments'] as $key => $values) {
+                $file[$key] = $values[$i];
+            }
+            $attachments[] = $file;
+        }
+    }
+}
+
 $controller = new TCPostController();
 
 $controller->authenticate_user();
@@ -22,6 +45,14 @@ $controller->authenticate_user();
 $new_post = null;
 if ($controller->can_create_post($thread_id)) {
     $new_post = $controller->create_post($thread_id, $post_content);
+
+    if (!empty($attachments)) {
+        foreach ($attachments as $file) {
+            if ($controller->can_add_attachment($new_post->post_id)) {
+                $controller->add_attachment($new_post->post_id, $file);
+            }
+        }
+    }
 }
 
 // Calculate the total pages in this thread so the user can be sent
